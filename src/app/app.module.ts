a8bf594220906aac1bb4ee0693b43496f4efb2f9
Maro <omaralonso27@gmail.com>
2021-11-17 23:59:38 -0500

    feat: Create the navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import {LogInComponent} from "./log-in/log-in.component";
-
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {HomeComponent} from "./home/home.component";
+import {HistoricComponent} from "./historic/historic.component";
+import {TestComponent} from "./test/test.component";
+import { MaterialModule } from './material/material.module';
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    HomeComponent,
+    HistoricComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
